517f0b2307f24ae46d81d46c9dfef83d91483ac2
mrniktarasov <nikitatarasov987@gmail.com>
2022-12-15 16:57:53 +0300

    Konva-test added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Stage, Layer, Circle, Text } from 'react-konva';
 import './App.css';
 
+const INITIAL_STATE = [];
+
 function App() {
+  const [stars, setStars] = useState(INITIAL_STATE);
+
+  const handleDragStart = (event) => {
+    const id = event.target.id();
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: star.id === id,
+        };
+      })
+    );
+  };
+
+  const handleDragEnd = (event) => {
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: false,
+        };
+      })
+    );
+  };
+
+  const handleClick = (event) => {
+    debugger
+    const a = event.target;
+    setStars([
+      ...stars,
+      {
+        id: `${event.target.pointerPos.x}---${event.target.pointerPos.y}`,
+        x: event.target.pointerPos.x,
+        y: event.target.pointerPos.y,
+        isDragging: false,
+      }
+    ])
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stage width={window.innerWidth} height={window.innerHeight} onClick={handleClick}>
+        <Layer>
+          <Text text="Try to click" />
+          {stars.map((star) => (
+            <Circle
+              key={star.id}
+              id={star.id}
+              x={star.x}
+              y={star.y}
+              radius={10}
+              opacity={0.8}
+              fill="black"
+              numPoints={5}
+              draggable
+              scaleX={star.isDragging ? 1.1 : 1}
+              scaleY={star.isDragging ? 1.1 : 1}
+              onDragStart={handleDragStart}
+              onDragEnd={handleDragEnd}
+            />
+          ))}
+        </Layer>
+      </Stage>
     </div>
   );
 }
